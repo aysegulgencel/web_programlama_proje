15b8d9614b3d74cf96fd60fbab5192c58919400c
aysegulgencel <aysegulgencel0@gmail.com>
2022-12-22 20:04:08 +0300

    shopapp.data Migrations dosyası eklendi.

---
+++ shopapp/shopapp.data/Migrations/20221217110514_addCategoryUrl.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopapp.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCategoryUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Products",
+                type: "REAL",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Categories",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Products",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "REAL",
+                oldNullable: true);
+        }
+    }
+}
---
+++ shopapp/shopapp.data/Migrations/20221218072953_AddColumnProductIsHome.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopapp.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnProductIsHome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHome",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsHome",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Products");
+        }
+    }
+}
